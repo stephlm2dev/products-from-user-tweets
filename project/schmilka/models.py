111159927ffd037d6bfa3a60046ee2b0a91687d8
Bouillaguet Quentin <quentin75012@gmail.com>
2015-12-11 10:05:00 +0100

    feat(models): Add IDF and Meta Database

--- project/schmilka/models.py
+++ project/schmilka/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class IDF(models.Model):	
+   
+    token = models.CharField(max_length = 50, primary_key = True)
+    nbDoc = models.IntegerField()
+
+    def __str__(self):
+        return self.token
+
+
+class Meta(models.Model):
+
+    key = models.CharField(max_length = 30, primary_key = True)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return self.key
